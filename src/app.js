929ee26a88d16677e06bd53bc83a301afd6322eb
om sakwan <omsakwan@gmail.com>
2026-07-15 16:03:22 +0530

    main project push

---
+++ src/app.js
@@ -0,0 +1,31 @@
+import express from "express";
+import cors from "cors";
+
+
+const app = express();
+//BASIC CONFIGURATIONS
+app.use(express.json({limit: "16kb"}));
+app.use(express.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+//CORS CONFIGURATION
+app.use(cors({
+    origin: process.env.CORS_ORIGIN ?.split(",") || "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS","PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+
+})
+);
+
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.get("/instagram", (req, res) => {
+    res.send("Instagram route");
+});
+
+
+export default app;
