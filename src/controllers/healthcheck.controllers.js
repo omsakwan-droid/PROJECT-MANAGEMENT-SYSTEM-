d71798e28d78a6e5f16267fd91cc2b6effcff7de
om sakwan <omsakwan@gmail.com>
2026-07-19 23:29:57 +0530

    feat: add User mongoose schema

--- src/controllers/healthcheck.controllers.js
+++ src/controllers/healthcheck.controllers.js
@@ -1,12 +1,17 @@
 import {ApiResponse} from "../utils/api-response.js";
+import {asyncHandler} from "../utils/async-handler.js";
+// const healthcheck=(req,res)=>{
+//     try{
+//       res.status(200).json(new ApiResponse(200,{message:"server is running"}))
+//     }
+//     catch(error){
 
-const healthcheck=(req,res)=>{
-    try{
-      res.status(200).json(new ApiResponse(200,{message:"server is running"}))
-    }
-    catch(error){
-
-    }
-};
+//     }
+// };
+const healthcheck=asyncHandler(async(req,res)=>{
+  res
+  .status(200)
+  .json(new ApiResponse(200,{message:"server is still running"}));
+})
 
 export {healthcheck};
